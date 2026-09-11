4ffa5aad5f99638a0442df08f620b9d70da6ce53
icarok99-alt <icarok00@gmail.com>
2026-09-11 16:19:07 -0300

    Update BASE_URL for DORAMAS ONLINE scraper

--- lib/scrapers/doramasonline.py
+++ lib/scrapers/doramasonline.py
@@ -14,7 +14,7 @@ _resolver = Resolver()
 
 WEBSITE = 'DORAMAS ONLINE'
 
-BASE_URL = 'https://doramasonline.org/'
+BASE_URL = 'https://doramasonline.net/'
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
